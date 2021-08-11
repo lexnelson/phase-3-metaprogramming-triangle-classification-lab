bbfd74e7dcd35f38970532fb474ad08677de7422
Lex Nelson <lexinelson597@gmail.com>
2021-08-11 12:04:54 -0400

    commit tree 3a6d2e554dd69df7645f99a54dcfcba772578f4f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a=a
+    @b=b
+    @c=c
+  end
+  def kind()
+    validate
+    if a == b && a == c
+      :equilateral
+    elsif a == b || b == c || c == a
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  def greater_than
+    a > 0 && b > 0 && c > 0
+  end
+  def side_lengths 
+    a + b > c && b + c > a && a + c > b
+  end
+  def validate 
+    raise TriangeError unless greater_than && side_lengths
+  end
+
+  class TriangeError < StandardError
+  end
+
 end
